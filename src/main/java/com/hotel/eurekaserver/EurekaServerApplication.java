6768952aed6341aebbae3df3ec80d001d9aa6e53
Rupesh <rupesh16495@gmail.com>
2024-04-29 18:16:20 -0700

    Initial commit

---
+++ src/main/java/com/hotel/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,10 @@
+package com.hotel.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args
